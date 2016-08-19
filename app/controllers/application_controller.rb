7a82c46133df56bce9afe4fe0c970bfa7e612145
Abigail <acdommer@gmail.com>
2016-08-19 14:38:04 -0700

    fixed error with require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def require_login
-  	redirect_to :root if session[:type] = nil
+  	redirect_to :root if !session[:role_id]
   end
 
   def require_correct_borrower
